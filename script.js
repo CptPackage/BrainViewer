fc6a514bb812774b8f56d9c1004eb8b491d3631d
CptPackage <abdelrhmanabdalla15@gmail.com>
2021-04-08 23:03:46 +0200

    Fixed Hover Overflow

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const hoverLabel = document.getElementById("hover-label");
+const modal = document.getElementById("article-modal");
 const sections = document.getElementsByClassName("section");
 const sectionsLength = sections.length;
 for (let i = 0; i < sectionsLength; i++) {
   sections[i].addEventListener("mouseover", ({ target }) => {
     const value = target.attributes["sectionname"].nodeValue;
     hoverLabel.innerText = value;
-    console.log("Hovered:", value);
+  });
+  sections[i].addEventListener("click", ({}) => {
+    modal.show();
   });
 }
