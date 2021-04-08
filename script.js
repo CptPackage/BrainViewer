2012df420da94e18bef1e6713f1d24454448bd97
CptPackage <abdelrhmanabdalla15@gmail.com>
2021-04-09 00:31:41 +0200

    Finished Inner View Structure

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+const modalElement = $("#article-modal");
+const modalCloseButton = document.getElementById("modal-close-btn");
+const modalTitle = document.getElementsByClassName("modal-title")[0];
 const hoverLabel = document.getElementById("hover-label");
-const modal = document.getElementById("article-modal");
 const sections = document.getElementsByClassName("section");
 const sectionsLength = sections.length;
+
 for (let i = 0; i < sectionsLength; i++) {
   sections[i].addEventListener("mouseover", ({ target }) => {
     const value = target.attributes["sectionname"].nodeValue;
     hoverLabel.innerText = value;
   });
-  sections[i].addEventListener("click", ({}) => {
-    modal.show();
+
+  sections[i].addEventListener("click", ({ target }) => {
+    const value = target.attributes["sectionname"].nodeValue;
+    modalTitle.innerHTML = value;
+    modalElement.modal("show");
   });
 }
+
+modalCloseButton.addEventListener("click", () => {
+  modalElement.modal("hide");
+});
